4e0763b9127f8a216e279823074bb7422e3bd256
SMDPRESONEL <152860366+SMDPRESONEL@users.noreply.github.com>
2023-12-04 17:47:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ from pyrogram import idle
 from lazybot import LazyPrincessBot
 from util.keepalive import ping_server
 from lazybot.clients import initialize_clients
-from laport import coroutine
+from asyncio import coroutine
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
